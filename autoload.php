695cabdb9b4508e6be8c8e68c82bbcd5afaf922f
thernoff <thernoff@yandex.ru>
2019-08-27 16:08:09 +0300

    Вместо функции _autoload() используется spl_autoload_register()

--- autoload.php
+++ autoload.php
@@ -1,11 +1,7 @@
 <?php
-/**
- * @param unknown $class
- */
-function __autoload($class)
-{
-	
-	if (file_exists(__DIR__ . '/' . str_replace('\\', '/', $class) . '.php')){
-		require __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';		
-	}
-}
+
+spl_autoload_register(function ($class) {
+    if (file_exists(__DIR__ . '/' . str_replace('\\', '/', $class) . '.php')) {
+        require __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+    }
+});
