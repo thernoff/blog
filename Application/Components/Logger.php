e8fc6a2429021481d4cd82c28b23513d9ac1d52a
thernoff <thernoff@yandex.ru>
2019-09-02 12:49:42 +0300

    Настройки для логов вынесены в конфиг. Класс Logger теперь при повторном вызове статического метода addError() возвращает раннее созданный объект данного класса.

--- Application/Components/Logger.php
+++ Application/Components/Logger.php
@@ -2,21 +2,41 @@
 
 namespace Application\Components;
 
+use Application\Core\Config;
+
 class Logger {
 
-    protected static $fileName = 'Application/logs/log.txt';
+    protected static $logger = null;
+    protected static $pathLogs = '';
     protected static $log;
 
     public function __construct() {
-        //self::$log = fopen($this->fileName, 'a');
+        if (null !== self::$logger) {
+            return self::$logger;
+        }
+
+        $config = Config::instance()->data;
+        $dirLog = $config['settings_logs']['dir_log'];
+        $fileLog = $config['settings_logs']['file_log'];
+
+        if (!@is_dir($dirLog)) {
+            mkdir($dirLog, 0770, true);
+        }
+
+        self::$pathLogs = $dirLog . "/" . $fileLog;
+        self::$log = fopen(self::$pathLogs, 'a');
     }
 
     public static function addError($error) {
-        $res = file_get_contents(self::$fileName);
-        //$res[] = $error;
-        $res .= $error . "\n";
-        //file_put_contents(self::$fileName, implode("\n", $res));
-        file_put_contents(self::$fileName, $res);
+        self::$logger = new self();
+
+        $file = file_get_contents(self::$logger->getPathLogs());
+        $file .= $error . "\n";
+        file_put_contents(self::$logger->getPathLogs(), $file);
+    }
+
+    protected function getPathLogs() {
+        return self::$pathLogs;
     }
 
 }
